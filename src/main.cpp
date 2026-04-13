d0628f47f2c0395cd9664c1068e80e7152e05bce
Grien25 <riengupta@gmail.com>
2026-04-13 16:01:15 -0700

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+
+// reeot - ReXGlue Recompiled Project
+//
+// This file is yours to edit. 'rexglue migrate' will NOT overwrite it.
+
+#include "generated/default_xex/reeot_config.h"
+#include "generated/default_xex/reeot_init.h"
+
+#include "reeot_app.h"
+
+REX_DEFINE_APP(reeot, ReeotApp::Create)
